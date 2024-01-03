61cc3eca9618b3052a1e998f09d0ed19f964efe9
林杨 <ly946546844@gmail.com>
2024-01-03 15:29:11 +0800

    feat：将登录信息写入redis，并且请求增加是否登录判断的过滤器

---
+++ web/src/main/java/com/yiwei/filter/JwtAuthenticationTokenFilter.java
@@ -0,0 +1,64 @@
+package com.yiwei.filter;
+
+import com.yiwei.utils.JwtUtil;
+import com.yiwei.utils.RedisCache;
+import com.yiwei.web.domain.login.LoginUser;
+import io.jsonwebtoken.Claims;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Objects;
+
+/**
+ * @author 35238
+ * @date 2023/7/12 0012 14:07
+ */
+@Component
+public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private RedisCache redisCache;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String xxtoken = request.getHeader("token");
+        // 判空，不一定所有的请求都有请求头，所以上面那行的xxtoken可能为空
+        if (!StringUtils.hasText(xxtoken)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        String userid;
+        try {
+            Claims claims = JwtUtil.parseJWT(xxtoken);
+            userid = claims.getSubject();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("token非法");
+        }
+        String redisKey = "login:" + userid;
+        LoginUser loginUser = (LoginUser) redisCache.getCacheObject(redisKey);
+        // redis里面可能并不存在这个用户信息，例如缓存过期了
+        if (Objects.isNull(loginUser)) {
+            throw new RuntimeException("用户未登录");
+        }
+
+        // 把最终的LoginUser用户信息，通过setAuthentication方法，存入SecurityContextHolder
+        // TODO 获取权限信息封装到Authentication中
+        UsernamePasswordAuthenticationToken authenticationToken =
+                // 第一个参数是LoginUser用户信息，第二个参数是凭证(null)，第三个参数是权限信息(null)
+                new UsernamePasswordAuthenticationToken(loginUser, null, null);
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+
+        // 全部做完之后，就放行
+        filterChain.doFilter(request, response);
+    }
+}
